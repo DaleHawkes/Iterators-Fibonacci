ef738fd383abed47ed910073ab1402750ddc9c98
Dale Giles <dale.giles@shearings.com>
2016-03-10 09:23:03 +0000

    Initial Commit

---
+++ Iterators Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterators_Fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (int fib in Fibs(6))
+            {
+                Console.Write(fib + " ");
+            }
+
+            static IEnumerable<int> Fibs(int fibCount)
+            {
+                for (int i = 0, prevFib = 1, curFib = 1; i < fibCount; i++)
+                {
+                    yield return prevFib;
+                    int newFib = prevFib + curFib;
+                    prevFib = curFib;
+                    curFib = newFib;
+                }
+            }
+        }
+    }
+}
